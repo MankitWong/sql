c3a06c494cd8b1dd5980961da8bdb6a1dbbc31bd
MankitWong <alston.wong@gmail.com>
2025-11-24 01:53:32 -0500

    add back the description
    Updated SQL query to use COALESCE for handling NULL values in product details.

--- 02_activities/assignments/Cohort_8/assignment2.sql
+++ 02_activities/assignments/Cohort_8/assignment2.sql
@@ -1,4 +1,25 @@
+/* ASSIGNMENT 2 */
+/* SECTION 2 */
 
+-- COALESCE
+/* 1. Our favourite manager wants a detailed long list of products, but is afraid of tables! 
+We tell them, no problem! We can produce a list with all of the appropriate details. 
+
+Using the following syntax you create our super cool and not at all needy manager a list:
+
+SELECT 
+product_name || ', ' || product_size|| ' (' || product_qty_type || ')'
+FROM product
+
+
+But wait! The product table has some bad data (a few NULL values). 
+Find the NULLs and then using COALESCE, replace the NULL with a blank for the first column with
+nulls, and 'unit' for the second column with nulls. 
+
+**HINT**: keep the syntax the same, but edited the correct components with the string. 
+The `||` values concatenate the columns into strings. 
+Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
+All the other rows will remain the same. */
 SELECT 
 COALESCE(product_name,'') || ', ' || COALESCE(product_size,'')|| ' (' || COALESCE(product_qty_type,'unit') || ')' [product_detail]
 FROM product;
